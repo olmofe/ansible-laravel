b2e13cbff84e2097650f0f796ce400beab45fd99
Boluwatife Morawo <boluwatife.morawo@stu.cu.edu.ng>
2022-11-02 18:18:03 +0100

    uncommented route

--- application/laravel-realworld/routes/web.php
+++ application/laravel-realworld/routes/web.php
@@ -13,6 +13,11 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-/*Route::get('/', function () {
+Route::get('/', function () {
     return view('welcome');
-});*/
+});
+
+Route::get('hello', function()
+  {
+      return 'Hello World!';
+  });
